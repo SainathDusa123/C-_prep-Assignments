3b7d25e58ba134174a184aecc3ca14b4dd5ec5a7
SainathDusa123 <sainathdussa1002@gmail.com>
2026-02-22 23:57:41 +0530

    commandline_1.cpp

--- commandline_1.cpp
+++ commandline_1.cpp
@@ -1,1 +1,25 @@
 #include <iostream>
+#include <cstdlib>   
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+        cout << "Please enter numbers as command line arguments." << endl;
+        return 1;
+    }
+
+    int max = atoi(argv[1]);
+
+    for (int i = 2; i < argc; i++) {
+        int num = atoi(argv[i]);
+
+        if (num > max) {
+            max = num;
+        }
+    }
+
+    cout << "Maximum number is: " << max << endl;
+
+    return 0;
+}
